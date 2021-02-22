1eef01aa049668dbbd00a3fbd5fb57c9c108fba6
Lennart Piro <piro.lennart@gmail.com>
2021-02-22 23:24:58 +0100

    replace ShiftRegister74HC595 class with my own simple implementation, plus a "stateful" version that remembers the current pin state

--- simple_74hc595.py
+++ simple_74hc595.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 """
-`adafruit_74hc595`
+`simple_74hc595`
 ====================================================
 
 CircuitPython driver for 74HC595 shift register.
@@ -22,27 +22,66 @@ Implementation Notes
 * Adafruit CircuitPython firmware for the supported boards:
   https://github.com/adafruit/circuitpython/releases
 
-* Adafruit's Bus Device library: https://github.com/adafruit/Adafruit_CircuitPython_BusDevice
 """
 
 import digitalio
-import adafruit_bus_device.spi_device as spi_device
 
-__version__ = "0.0.0-auto.0"
-__repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_74HC595.git"
+__version__ = "1.0.0-auto.0"
+__repo__ = "https://github.com/LennartPiro/simple_CircuitPython_74HC595"
+
+
+class simple_74hc595:
+    def __init__(self, data_pin, clock_pin, latch_pin):
+        self.data_pin = digitalio.DigitalInOut(data_pin)
+        self.data_pin.direction = digitalio.Direction.OUTPUT
+        self.clock_pin = digitalio.DigitalInOut(clock_pin)
+        self.clock_pin.direction = digitalio.Direction.OUTPUT
+        self.latch_pin = digitalio.DigitalInOut(latch_pin)
+        self.latch_pin.direction = digitalio.Direction.OUTPUT
+
+        self.end_push_values()
+
+    def push_bit(self, value):
+        self.clock_pin.value = False
+        self.data_pin.value = value
+        self.clock_pin.value = True
+
+    def begin_push_values(self):
+        self.clock_pin.value = False
+        self.latch_pin.value = False
+        self.clock_pin.value = True
+
+    def end_push_values(self):
+        self.clock_pin.value = False
+        self.latch_pin.value = True
+        self.clock_pin.value = True
+
+    def write_byte(self, value):
+        assert 0 <= value <= 255
+        self.begin_push_values()
+        for i in range(8):
+            self.push_bit((value >> (7 - i)) & 1)
+        self.end_push_values()
+
+    def write_byte_array(self, values):
+        assert len(values) == 8
+        self.begin_push_values()
+        for v in values:
+            self.push_bit(v)
+        self.end_push_values()
 
 
 class DigitalInOut:
-    """Digital input/output of the 74HC595.  The interface is exactly the
-    same as the ``digitalio.DigitalInOut`` class, however note that by design
-    this device is OUTPUT ONLY!  Attempting to read inputs or set
-    direction as input will raise an exception.
-    """
+    # """Digital input/output of the 74HC595.  The interface is exactly the
+    # same as the ``digitalio.DigitalInOut`` class, however note that by design
+    # this device is OUTPUT ONLY!  Attempting to read inputs or set
+    # direction as input will raise an exception.
+    # """
 
     def __init__(self, pin_number, shift_register_74hc595):
-        """Specify the pin number of the shift register (0...7) and
-        ShiftRegister74HC595 instance.
-        """
+        # """Specify the pin number of the shift register (0...7) and
+        # ShiftRegister74HC595 instance.
+        # """
         self._pin = pin_number
         self._shift_register = shift_register_74hc595
 
@@ -50,80 +89,66 @@ class DigitalInOut:
     # with DigitalInout class (which allows specifying pull, etc. which
     # is unused by this class).  Do not remove them, instead turn off pylint
     # in this case.
-    # pylint: disable=unused-argument
     def switch_to_output(self, value=False, **kwargs):
-        """``DigitalInOut switch_to_output``"""
         self.direction = digitalio.Direction.OUTPUT
         self.value = value
 
-    def switch_to_input(self, **kwargs):  # pylint: disable=no-self-use
-        """``switch_to_input`` is not supported."""
+    def switch_to_input(self, **kwargs):
         raise RuntimeError("Digital input not supported.")
-
-    # pylint: enable=unused-argument
 
     @property
     def value(self):
-        """The value of the pin, either True for high or False for low."""
-        return self._shift_register.gpio & (1 << self._pin) == (1 << self._pin)
+        return self._shift_register.read_bit(self._pin)
 
     @value.setter
     def value(self, val):
-        gpio = self._shift_register.gpio
-        if val:
-            gpio |= 1 << self._pin
-        else:
-            gpio &= ~(1 << self._pin)
-        self._shift_register.gpio = gpio
+        self._shift_register.write_bit(self._pin, val)
 
     @property
     def direction(self):
-        """``Direction`` can only be set to ``OUTPUT``."""
         return digitalio.Direction.OUTPUT
 
     @direction.setter
-    def direction(self, val):  # pylint: disable=no-self-use
-        """``Direction`` can only be set to ``OUTPUT``."""
+    def direction(self, val):
         if val != digitalio.Direction.OUTPUT:
             raise RuntimeError("Digital input not supported.")
 
     @property
     def pull(self):
-        """Pull-up/down not supported, return None for no pull-up/down."""
         return None
 
     @pull.setter
-    def pull(self, val):  # pylint: disable=no-self-use
-        """Only supports null/no pull state."""
+    def pull(self, val):
         if val is not None:
             raise RuntimeError("Pull-up and pull-down not supported.")
 
 
-class ShiftRegister74HC595:
-    """Initialise the 74HC595 on specified SPI bus."""
+class stateful_74hc595(simple_74hc595):
+    def __init__(self, data_pin, clock_pin, latch_pin, initial_state=0):
+        super().__init__(
+            data_pin, clock_pin, latch_pin
+        )
+        self.pin_state = initial_state
+        self.update()
 
-    def __init__(self, spi, latch):
-        self._device = spi_device.SPIDevice(spi, latch, baudrate=1000000)
-        self._gpio = bytearray(1)
-        self._gpio[0] = 0x00
+    def update(self):
+        self.write_byte(self.pin_state)
 
-    @property
-    def gpio(self):
-        """The raw GPIO output register.  Each bit represents the
-        output value of the associated pin (0 = low, 1 = high).
-        """
-        return self._gpio[0]
+    def write_bit(self, pin_number, value):
+        assert 0 <= pin_number <= 7
+        if value:
+            self.pin_state |= 1 << pin_number
+        else:
+            self.pin_state ^= self.pin_state & (1 << pin_number)
+        self.update()
 
-    @gpio.setter
-    def gpio(self, val):
-        self._gpio[0] = val & 0xFF
-        with self._device as spi:
-            # pylint: disable=no-member
-            spi.write(self._gpio)
+    def read_bit(self, pin_number):
+        assert 0 <= pin_number <= 7
+        return (self.pin_state & (1 << pin_number)) > 0
 
     def get_pin(self, pin):
-        """Convenience function to create an instance of the DigitalInOut class
-        pointing at the specified pin of this 74HC595 device .
-        """
+        # """Convenience function to create an instance of the DigitalInOut class
+        # pointing at the specified pin of this 74HC595 device .
+        # """
         assert 0 <= pin <= 7
-        return DigitalInOut(pin, self)
+        return DigitalInOut74HC595(pin, self)
